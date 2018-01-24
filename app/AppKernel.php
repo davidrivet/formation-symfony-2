6491657cc8363d0ecb2924b2090c27664e4858d9
David RIVET <davidrivet@hotmail.fr>
2018-01-24 20:17:30 +0100

    création des nouveaux bundles Formation/ExamBundle et Formation/ExamAdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Formation\ExamBundle\FormationExamBundle(),
+            new Formation\ExamAdminBundle\FormationExamAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
