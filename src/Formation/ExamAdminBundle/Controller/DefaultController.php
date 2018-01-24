166a7975f6dee420550004ceced6e8e342076db1
David RIVET <davidrivet@hotmail.fr>
2018-01-24 22:20:55 +0100

    playing with the routes declarations order

--- src/Formation/ExamAdminBundle/Controller/DefaultController.php
+++ src/Formation/ExamAdminBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FormationExamAdminBundle:Default:index.html.twig');
+        return $this->render('FormationExamAdminBundle:Default:index.json.twig');
     }
 }
