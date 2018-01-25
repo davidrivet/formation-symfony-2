474dec8862982b4696d5406bfe6c9a8738d5570c
David Rivet <davidrivet@hotmail.fr>
2018-01-25 23:39:40 +0100

    another route created with the annotation method

--- src/Formation/ExamBundle/Controller/DefaultController.php
+++ src/Formation/ExamBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Formation\ExamBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,5 +15,12 @@ class DefaultController extends Controller
 
     public function listExamsAction() {
         return new Response('The available exams will be listed here.');
+    }
+
+    /**
+     * @Route("/exam/{id}/desc", requirements={"id" = "\d+"}, defaults={"id" = 1}, name="description")
+     */
+    public function showExamDescriptionAction($id) {
+        return new Response("Description of the exam $id will be put here.");
     }
 }
