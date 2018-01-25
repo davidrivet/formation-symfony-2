e494bac2380aed4a20b9720632969ed8cf8b283f
David Rivet <davidrivet@hotmail.fr>
2018-01-25 23:15:21 +0100

    /exams/list route added

--- src/Formation/ExamBundle/Controller/DefaultController.php
+++ src/Formation/ExamBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Formation\ExamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FormationExamBundle:Default:index.html.twig');
+    }
+
+    public function listExamsAction() {
+        return new Response('The available exams will be listed here.');
     }
 }
