825decaff7201e58db4dcf294a61998b2ed47c82
dariv <dariv@smile.fr>
2018-02-03 00:26:14 +0100

    prefix added to formation exam admin bundle routes

--- src/Formation/ExamAdminBundle/Controller/DefaultController.php
+++ src/Formation/ExamAdminBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace Formation\ExamAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('FormationExamAdminBundle:Default:index.json.twig');
+    }
+
+    public function listExamsAction() {
+        return new Response('The available exams will be listed here [admin mode].');
     }
 }
