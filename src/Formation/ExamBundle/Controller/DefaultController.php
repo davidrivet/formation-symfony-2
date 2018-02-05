8211518d2c0b2286f70468c6fd324a4ca24f5a27
dariv <dariv@smile.fr>
2018-02-05 23:00:00 +0100

    code modified to show exams list on page /exams/list using sample exams from mock data service

--- src/Formation/ExamBundle/Controller/DefaultController.php
+++ src/Formation/ExamBundle/Controller/DefaultController.php
@@ -18,13 +18,28 @@ class DefaultController extends Controller
     }
 
     public function listExamsAction() {
-        return new Response('The available exams will be listed here.');
+        // * reponse initiale
+        // * mauvaise pratique, transgression du principe mvc
+        // * préférer l'utilisation de templates
+        //return new Response('The available exams will be listed here.');
+
+        $sampleExams = $this->getSampleExams();
+
+        return $this->render("FormationExamBundle:Default:examsList.html.twig", array("exams"=>$sampleExams));
     }
 
     /**
-     * @Route("/exam/{id}/desc", requirements={"id" = "\d+"}, defaults={"id" = 1}, name="description")
+     * @Route("/exam/{id}/desc", requirements={"id" = "\d+"}, defaults={"id" = 1}, name="formation_exam_desc")
      */
     public function showExamDescriptionAction($id) {
         return new Response("Description of the exam $id will be put here.");
+    }
+
+    /**
+    * shortcut to the mock data service
+    */
+    private function getSampleExams() {
+      // * recuperation des donnees de test via le service cree pour
+      return $this->get('formation.sampledataservice')->getSampleExams();
     }
 }
